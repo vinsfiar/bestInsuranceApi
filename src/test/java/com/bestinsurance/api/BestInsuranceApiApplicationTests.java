80e3d35a28b19988191b91fb65d2e65ffe3e66fa
vinsfiar <enzofiar@gmail.com>
2024-06-26 14:02:48 +0100

    Add files via upload
    Completed Project Creation and Maven/Gradle Configuration

---
+++ src/test/java/com/bestinsurance/api/BestInsuranceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bestinsurance.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestInsuranceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

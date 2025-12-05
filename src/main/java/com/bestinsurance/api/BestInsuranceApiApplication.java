fb92505328e9b5586a822e5e650c4e386e43e6f4
vins <enzofiar@gmail.com>
2025-12-05 11:00:43 +0100

    Milestone 1: Project Creation and Maven/Gradle Configuration
    Objective
    - Set up the project using Spring Initializr
    - Configure Maven and Gradle to build the project
    - Add dependencies not provided by Spring Boot

---
+++ src/main/java/com/bestinsurance/api/BestInsuranceApiApplication.java
@@ -0,0 +1,13 @@
+package com.bestinsurance.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestInsuranceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestInsuranceApiApplication.class, args);
+	}
+
+}

1fb5c4ac660eb4a9f3e1e653ea004f648f9b5f21
vins <enzofiar@gmail.com>
2025-12-10 16:48:07 +0100

    Milestone 1: Authorization Server Objective - Implement an authorization server for testing purposes. Importance to project - Developing a local authentication server will be useful to test the API security that will be developed in the next milestones.

---
+++ authentication_server/src/test/java/com/bestinsurance/oauth2/Oauth2TestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bestinsurance.oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2TestServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
